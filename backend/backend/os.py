248c3d64572ebe56ab2c8e52aa0fee30ebc40900
Baljor Dhillon <baljor20@gmail.com>
2024-04-14 23:56:54 -0700

    os

---
+++ backend/backend/os.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["DATABASE_NAME"] ="fitnesshub"
+os.environ["USER"] = "root"
+os.environ["PASS"] = "159159"
+os.environ["HOST"] = "localhost"
+os.environ["PORT"] = "5432"
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,9 +75,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'fitnesshub',
+
+        'USER': '<db_username>',
+
+        'PASSWORD': '<password>',
+
+        'HOST': '<db_hostname_or_ip>',
+
+        'PORT': '<db_port>',
     }
 }
 
@@ -122,18 +132,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'fitnesshub',
-
-        'USER': '<db_username>',
-
-        'PASSWORD': '<password>',
-
-        'HOST': '<db_hostname_or_ip>',
-
-        'PORT': '<db_port>',
-    }
-}
