e2ede092ec10b88a58c68a8d768cf9475c200e01
baljor <baljor20@gmail.com>
2024-04-30 18:34:46 -0700

    learning mutations

--- server/polls/scheme.py
+++ server/polls/scheme.py
@@ -1,12 +1,36 @@
 import graphene
 from graphene_django import DjangoObjectType
 from polls.models import CustomUser
+import hashlib
 
 class CustomUserType(DjangoObjectType):
 
     class Meta:
         model = CustomUser
         field = "__all__"
+
+class customUserMutation(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required = True)
+        password = graphene.String(required = True)
+        email = graphene(required= True)
+
+    customuser = graphene.Field(CustomUserType)
+    @classmethod
+    def mutate(cls,root,info,email, username, password):
+        cus =cls.customuser
+        salt  = hashlib.sha256()
+        cus.salt = salt
+        hashpassword = salt.update(password)
+        hashpassword = hashpassword.hexdigest()
+        cus.hashed_pass = hashpassword
+        cus.email = email
+        cus.username = username
+        cus.is_premium = False
+
+        return customUserMutation(customuser = cus)
+
+        
 
 class Query(graphene.ObjectType):
     all_users = graphene.List(CustomUserType)
